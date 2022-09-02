6753ef72bcb515901b97d28dbdbd2c49b78072b6
Fekete-Bruno <brunohffekete@gmail.com>
2022-09-02 20:36:25 -0300

    chore: installed all dependencies

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 import express from 'express';
 import cors from 'cors';
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+import joi from "joi";
+dotenv.config();
+
 
 const app = express();
 app.use(cors());
